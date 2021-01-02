4338a1cbe9603e07f0aa8f9611d92c9013c18e92
tucelek@gmail.com <tucelek@gmail.com>
2021-01-02 14:39:46 +0100

    search add + fixes

--- script.js
+++ script.js
@@ -1,49 +1,83 @@
 const h1 = document.querySelector('h1')
-const wrapper = document.querySelector('.wrapper')
+const charactersWrap = document.querySelector('.characters-wrap')
+const search = document.getElementById('search')
+let characters = []
 
-loadCharacters()
+search.addEventListener('input', e => {
+  const searchString = e.target.value.toLowerCase()
+  const filteredCharacters = characters.filter(character => {
+    return (
+      character.name.toLowerCase().includes(searchString) ||
+      character.house.toLowerCase().includes(searchString) ||
+      character.ancestry.toLowerCase().includes(searchString)
+    )
+  })
+  displayCharacters(filteredCharacters)
+})
 
 async function loadCharacters() {
   const res = await fetch('https://hp-api.herokuapp.com/api/characters')
-  const data = await res.json()
-
-  for (const charData of data) {
-    createCharacter(charData)
-  }
+  characters = await res.json()
+  displayCharacters(characters)
 }
 
-function createCharacter(data) {
-  const a = document.createElement('div')
-  a.setAttribute('class', 'single-character')
-  a.innerHTML = `
-  <div class="flex">
-    <div class="name">${data.name}</div>
-    <div class="house">house: ${data.house}</div>
-  </div>
+loadCharacters()
 
-  <div class="photo">
-    <img src="${data.image}" alt="">
-  </div>
-  
-  <div class="additional-info">
-    <div class="left">
-      <div class="hover-name">
-        <h2>${data.name}</h2>
-        <div class="ancestry"><p>ancestry: </p>${data.ancestry}</div>
-        <div class="date-of-birth"><p>date of birth: </p>${data.dateOfBirth}</div>
-        <div class="patronus"><p>patronus: </p>${data.patronus}</div>
+function displayCharacters(characters) {
+  const htmlString = characters
+    .map(character => {
+      return `
+      <div class="single-character">
+      <div class="flex">
+        <div class="name">${character.name}</div>
+        <div class="house">house: ${character.house}</div>
+      </div>
+    
+      <div class="photo">
+        <img src="${character.image}" alt="" />
+      </div>
+    
+      <div class="additional-info">
+        <div class="left">
+          <div class="hover-name">
+            <h2>${character.name}</h2>
+            <div class="ancestry">
+              <p>ancestry:</p>
+              ${character.ancestry}
+            </div>
+            <div class="date-of-birth">
+              <p>date of birth:</p>
+              ${character.dateOfBirth}
+            </div>
+            <div class="patronus">
+              <p>patronus:</p>
+              ${character.patronus}
+            </div>
+          </div>
+        </div>
+    
+        <div class="right">
+          <div class="wand">
+            <h2>wand:</h2>
+            <div class="wood">
+              <p>wood:</p>
+              ${character.wand.wood}
+            </div>
+            <div class="core">
+              <p>core:</p>
+              ${character.wand.core}
+            </div>
+            <div class="length">
+              <p>length:</p>
+              ${character.wand.length}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+    `
+    })
+    .join('')
 
-    <div class="right">
-      <div class="wand">
-        <h2>wand:</h2>
-        <div class="wood"><p>wood: </p>${data.wand.wood}</div>
-        <div class="core"><p>core: </p>${data.wand.core}</div>
-        <div class="length"><p>length: </p>${data.wand.length}</div>
-      </div>
-    </div>
-  </div>
-  `
-  wrapper.appendChild(a)
+  charactersWrap.innerHTML = htmlString
 }
